396164b51a8e35aedd5dfe4e46e9cd5bee2fc52f
Florian Rouanet <rouanetflorian@gmail.com>
2021-07-08 16:37:57 +0200

    comment

--- backend/index.mjs
+++ backend/index.mjs
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config(); // Variable d'environnement
 console.log(process.env.PORT);
 
-// Connexion à MongoDB sécurisée
+// Connexion à MongoDB sécurisée / COURS : https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466348-configurez-votre-base-de-donnees
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.moa2t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
@@ -28,11 +28,11 @@ const app = express();
 // Middlewares se plaçant entre modules et fonctions :
 app.use(express.json()); // Permet de recevoir des corps de requête en JSON
 app.use(helmet()); // Module de sécurité évitant certaines formes d'attaques
-// Permission CORS
+// Tuto middleware appliqué à toutes les routes avec permission CORS (de base empêche les requêtes malveillantes d'accéder à des ressources)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Tout le monde a le droit d'accéder à l'API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Authorisation d'utiliser certaines en-têtes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Authorisation d'utiliser certaines méthodes
     next();
 });
 
@@ -45,3 +45,24 @@ app.post("/api/auth/signup", (req, res, next) => {
 // Connexion au port backend
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Serveur actif sur le port " + PORT)); // Le serveur Node va tourner continuellement
+
+/*
+    "bcrypt": "^5.0.1",
+    "body-parser": "^1.19.0",
+    "express": "^4.17.1",
+    "jsonwebtoken": "^8.5.1",
+    "mongoose": "^5.12.13",
+    "mongoose-unique-validator": "^2.0.3",
+    "multer": "^1.4.2"
+
+    app.post("/api/auth/signup", (req, res, next) => {
+    delete_req.body.id;
+    const thing = new Thing({
+      ...req.body,
+    });
+    thing
+      .save()
+      .then(() => res.status(201).json({ message: "Objet enregistré" }))
+      .catch((error) => res.status(400).json({ error }));
+  });
+*/
