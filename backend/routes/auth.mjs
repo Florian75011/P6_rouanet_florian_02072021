fdff2c09f93c9e9c5f38032df8f1183abb1b3851
Florian Rouanet <rouanetflorian@gmail.com>
2021-08-04 18:01:21 +0200

    email-hashed

--- backend/routes/auth.mjs
+++ backend/routes/auth.mjs
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import User from "../models/user.mjs"; // import du modèle du User en modèle
+import base64 from 'base-64'
 
 dotenv.config(); // Permet d'user des .env
 
@@ -50,8 +51,9 @@ function isValidPassword(value) {
 // Possibilité de création de comptes inscrits uniques
 export async function signUp(req, res, next) {
   try {
-    const hash = bcrypt.hashSync(req.body.password, 10); // Hashé le MDP côté back pour ne rien stocker dans la data base
-    const user = new User({ email: req.body.email, password: hash });
+    const hash = await bcrypt.hash(req.body.password, 10); // Hashé le MDP côté back pour ne rien stocker dans la data base
+    const hashE = await bcrypt.hash(req.body.email, 10); // Hashé le email de la même façon
+    const user = new User({ email: hashE, password: hash });
     await user.save();
     res.status(201).json({ message: "Inscription de l'utilisateur réussie" }); // 201 dit que le compte est créé avec succès
   } catch (err) {
@@ -68,32 +70,30 @@ export async function signUp(req, res, next) {
 export async function logIn(req, res, next) {
   try {
     // Vérifier que cet email existe
-    const searchUser = await User.findOne({ email: req.body.email }); // Fonction MongoDB qui récupère l'élément ajouté au paramètre
-    if (searchUser != null) {
-      // Comparer les mots de passe
-      const comparePassword = await bcrypt.compareSync(
-        req.body.password,
-        searchUser.password
-      ); // Renvoie true ou false
-      if (comparePassword === true) {
-        // Créer le token, et envoyer une réponse
-        const obj = {
-          // Créer un objet prenant en compte un userId avec le résultat du searchUser + le token secret
-          userId: "" + searchUser._id, // Contenu d'un jeton: user de la doc API + ID de MongoDB
-          token: jwt.sign(
-            { userId: searchUser._id },
-            process.env.TOKEN_SECRET,
-            { expiresIn: "24h" }
-          ), // L'utilisateur existe vraiment, donc on lui renvoie un jeton/token ; Crypte information pour la décrypter ensuite
-        };
-        console.log(obj);
-        res.status(200).json(obj);
-      } else {
-        res.status(401).json({ message: "Problème d'identification" }); // Renvoie une erreur 401 qui est une erreur d'authenfication
-      }
-    } else {
-      res.status(400).json({ message: "Utilisateur introuvable" }); // Renvoie une erreur 400 de paramètres
-    }
+    const searchUser = await User.find({}).lean(); // Chercher les utilisateurs
+    const userConnected = searchUser.find(function(user){
+      if(bcrypt.compareSync(req.body.email, user.email)) return user;
+      return false;
+    })
+    // console.log(userConnected);
+    if(!userConnected) {
+      return res.status(400).json({ message: "Utilisateur introuvable" });
+    };
+    if (!bcrypt.compareSync(req.body.password, userConnected.password)) {
+      return res.status(400).json({ message: "Utilisateur introuvable" });
+    };
+    // Créer le token, et envoyer une réponse
+    const obj = {
+      // Créer un objet prenant en compte un userId avec le résultat du searchUser + le token secret
+      userId: "" + userConnected._id, // Contenu d'un jeton: user de la doc API + ID de MongoDB
+      token: jwt.sign(
+        { userId: userConnected._id },
+        process.env.TOKEN_SECRET,
+        { expiresIn: "24h" }
+      ), // L'utilisateur existe vraiment, donc on lui renvoie un jeton/token ; Crypte information pour la décrypter ensuite
+    };
+    // console.log(obj);
+    res.status(200).json(obj);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Une erreur s'est produite" }); // Ne pas l'identifier précisémment pour ne pas aider les pirates informatiques
